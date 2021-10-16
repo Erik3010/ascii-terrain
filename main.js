295c7b0ac1c187761f5f5a7e537eb128adb815a7
Erik <erikhau3010@gmail.com>
2021-10-17 01:54:30 +0700

    feat: add input styling

--- main.js
+++ main.js
@@ -2,8 +2,6 @@ import "./style.css";
 
 import tests from "./tests/index";
 
-console.clear();
-
 const solution = (patternString) => {
   const CHAR = {
     "-": "_",
@@ -28,15 +26,15 @@ const solution = (patternString) => {
   pattern.forEach((item, index) => {
     if (!result[pointer.y]) result[pointer.y] = [];
 
-    if (index > 0 && getInc(pattern[index - 1], item)) {
+    if (index > 0 && getInc(pattern[index - 1], item))
       pointer.y += getInc(pattern[index - 1], item);
-    }
+
     if (pointer.y < 0) {
       result.unshift(Array(pointer.x).fill(" "));
       pointer.y = 0;
-    } else if (pointer.y > result.length - 1) {
+    } else if (pointer.y > result.length - 1)
       result.push(Array(pointer.x).fill(" "));
-    }
+
     result[pointer.y][pointer.x] = CHAR[item];
 
     result.forEach((_, row) => {
